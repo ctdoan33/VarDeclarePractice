716a06430bf567c1ec55250d7537f4ceef879d4f
ctdoan33 <doanchristopher_1@yahoo.com>
2017-08-18 16:20:14 -0700

    initial commit

---
+++ VarDeclarePractice.js
@@ -0,0 +1,24 @@
+// Numbers
+var natural = 42;
+console.log("Just a number", natural);
+var integer = -21;
+console.log("Negative number", integer);
+var fraction = 1/7;
+console.log("Fraction", fraction);
+var irrational = Math.sqrt(2);
+console.log("Root 2", irrational);
+// Strings
+var location = "CodingDojo";
+console.log("Where we are", location);
+var date = "August 8";
+console.log("Today's date", date);
+var punctuation = "!()-:;,.?";
+console.log("punctuation symbols", punctuation);
+// Booleans
+var wearehuman = true;
+console.log("Human", wearehuman);
+var iamrich = false;
+console.log("Rich", iamrich);
+// Undefined
+var nothing;
+console.log("error", nothing);
